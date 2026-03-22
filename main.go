deb45e5dee0f14a1ce81b61e304de15f81d95ab0
a20n21 <andersonmatias188@outlook.com>
2026-03-22 16:03:18 -0300

    feat: adiciona endpoint /metrics para observabilidade com Prometheus

--- main.go
+++ main.go
@@ -1,15 +1,15 @@
+package main
+
 import (
     "net/http"
     "github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
-   
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         w.Write([]byte("Hello ARGOCD, foram feitas novas alterações nessa aplicação"))
     })
 
-    
     http.Handle("/metrics", promhttp.Handler())
 
     http.ListenAndServe(":8080", nil)
